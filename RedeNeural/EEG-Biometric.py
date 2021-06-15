3f47cf81905750e5455fa36f3351535f77e2dd98
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-06-15 11:04:43 -0300

    .

--- RedeNeural/EEG-Biometric.py
+++ RedeNeural/EEG-Biometric.py
@@ -150,7 +150,7 @@ def pre_processing(content, filter_option = 3):
     # Normalizing the signal
     while c < channels:
         content[c] -= np.mean(content[c])
-        content[c] += np.absolute(np.amin(content[c]))
+        # content[c] += np.absolute(np.amin(content[c]))
         content[c] /= np.std(content[c])
         content[c] /= np.amax(content[c])
         c += 1
