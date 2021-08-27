3be5f2aca85775cbfb1b9d8505f01c7a91500435
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-08-27 00:17:51 -0300

    tentativa LSTM

--- RedeNeural/models.py
+++ RedeNeural/models.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1
 from tensorflow.keras.layers import Concatenate, GlobalAveragePooling1D, Reshape, Activation, Permute, Multiply
 from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization, Bidirectional, LSTM, GRU
 from tensorflow.keras import Input, Model
-import tensorflow.keras.backend as K
+from tensorflow import convert_to_tensor
 
 def scheduler(current_epoch, learning_rate):
     """
@@ -282,7 +282,7 @@ def create_model_LSTM(window_size, num_channels, num_classes, remove_last_layer=
     #x = LSTM(128, return_sequences=True) (x)
     #x = LSTM(128, return_sequences=True) (x)
     #x = LSTM(128, return_sequences=True) (x)
-    x = K.constant(inputs)
+    x = convert_to_tensor(inputs)
     x = Bidirectional(LSTM(128), return_sequences=True) (x)
     x = Bidirectional(LSTM(128)) (x)
 
