644f4f40f483b633f876139233a681cbfaf99c49
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-08-27 00:16:10 -0300

    tentativa LSTM

--- RedeNeural/models.py
+++ RedeNeural/models.py
@@ -283,6 +283,7 @@ def create_model_LSTM(window_size, num_channels, num_classes, remove_last_layer=
     #x = LSTM(128, return_sequences=True) (x)
     #x = LSTM(128, return_sequences=True) (x)
     x = K.constant(inputs)
+    x = Bidirectional(LSTM(128), return_sequences=True) (x)
     x = Bidirectional(LSTM(128)) (x)
 
     x = Flatten() (x)
