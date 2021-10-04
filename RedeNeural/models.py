dbba8c212fddddd3dbc1c4749c30d5e880b535c4
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-10-04 15:27:12 -0300

    sun model

--- RedeNeural/models.py
+++ RedeNeural/models.py
@@ -327,13 +327,17 @@ def create_model_LSTM(window_size, num_channels, num_classes, remove_last_layer=
 
     model.add(Input(shape=(window_size, num_channels)))
     model.add(Bidirectional(LSTM(10, return_sequences=True)))
-    model.add(Bidirectional(LSTM(10)))
-    model.add(Dense(5))
+    model.add(Bidirectional(LSTM(10, return_sequences=True)))
+    model.add(Bidirectional(LSTM(10, return_sequences=True)))
+    model.add(Bidirectional(LSTM(10, return_sequences=True)))
+    model.add(Bidirectional(LSTM(10, return_sequences=True)))
+    model.add(Flatten())
+    model.add(Dense(256))
 
     # Model used for Identification
     if(remove_last_layer == False):
-        model.add(BatchNormalization())
-        model.add(Dropout(0.1))
+        # model.add(BatchNormalization())
+        # model.add(Dropout(0.1))
         model.add(Dense(num_classes, activation='softmax'))
 
     return model
@@ -351,29 +355,27 @@ def create_model_GRU(window_size, num_channels, num_classes, remove_last_layer=F
         softmax activation function.
     """
 
-    inputs = Input(shape=(window_size, num_channels))
-    
-    x = Bidirectional(GRU(128, return_sequences=True)) (inputs)
-    x = Bidirectional(GRU(128, return_sequences=True)) (x)
-    x = Bidirectional(GRU(128, return_sequences=True)) (x)
-    x = Bidirectional(GRU(128, return_sequences=True)) (x)
-    x = Bidirectional(GRU(128)) (x)
+    model = None
 
-    x = Flatten() (x)
-    x = Dense(4096)(x)
-    x = Dense(4096)(x)
-    x = Dense(256)(x)
+    if(remove_last_layer == False):
+        model = Sequential(name='Biometric_for_Identification')
+    else:
+        model = Sequential(name='Biometric_for_Verification')
+
+    model.add(Input(shape=(window_size, num_channels)))
+    model.add(Bidirectional(GRU(10, return_sequences=True)))
+    model.add(Bidirectional(GRU(10, return_sequences=True)))
+    model.add(Bidirectional(GRU(10, return_sequences=True)))
+    model.add(Bidirectional(GRU(10, return_sequences=True)))
+    model.add(Bidirectional(GRU(10, return_sequences=True)))
+    model.add(Flatten())
+    model.add(Dense(256))
 
     # Model used for Identification
     if(remove_last_layer == False):
-        x = BatchNormalization()(x)
-        x = Dropout(0.1) (x)
-        x = Dense(num_classes, activation='softmax') (x)
-        model = Model(inputs=inputs, outputs=x, name='Biometric_for_Identification')
-        
-    # Model used for Verification
-    else:
-        model = Model(inputs=inputs, outputs=x, name='Biometric_for_Verification')
+        # model.add(BatchNormalization())
+        # model.add(Dropout(0.1))
+        model.add(Dense(num_classes, activation='softmax'))
 
     return model
 
