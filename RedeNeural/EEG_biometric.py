468463954d3f0989477b960aac9f88d5d92c98e5
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-10-06 23:24:10 -0300

    gru model

--- RedeNeural/EEG_biometric.py
+++ RedeNeural/EEG_biometric.py
@@ -99,7 +99,7 @@ all_channels_yang = ['C1..', 'Cz..', 'C2..', 'Af3.', 'Afz.', 'Af4.', 'O1..', 'Oz
 #
 # apenas GRU - 5 blocos bidirecionais com 10 units cada   - 67,3395% acurácia, 48,9588% EER e 0.0734 Decidibilidade
 # apenas GRU - 5 blocos com 10 units cada                 - 64,7706% acurácia, 48,3384% EER e 0.0696 Decidibilidade
-# GRU entre Pool3 e flatten do schons - 5 blocos bidirecionais com 10 units cada - XX,XXXX% acurácia, XX,XXXX% EER e X.XXXX Decidibilidade
+# GRU entre Pool3 e flatten do schons - 5 blocos bidirecionais com 10 units cada - 58,3486% acurácia, 19,0014% EER e 1.7760 Decidibilidade
 # GRU antes de Conv1 do schons        - 5 blocos bidirecionais com 10 units cada - XX,XXXX% acurácia, XX,XXXX% EER e X.XXXX Decidibilidade
 
 # functions.create_csv_database_from_edf('./Dataset/','./Dataset_CSV/', num_classes)
