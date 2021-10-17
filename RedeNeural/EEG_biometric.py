bd3bfa9b20abcd56d0850cd0195a13f8cae20c74
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-10-17 16:29:40 -0300

    protocolo p1 yang

--- RedeNeural/EEG_biometric.py
+++ RedeNeural/EEG_biometric.py
@@ -17,8 +17,8 @@ initial_learning_rate = 0.01    # Initial learning rate
 # Parameters used in functions.load_data()
 # folder_path = './Dataset_CSV/'
 folder_path = '/media/work/carlosfreitas/IniciacaoCientifica/RedeNeural/Dataset_CSV/'
-train_tasks = [1]               # Tasks used for training and validation
-test_tasks = [2]                # Tasks used for testing
+train_tasks = [3, 11]               # Tasks used for training and validation
+test_tasks = [7]                # Tasks used for testing
 num_classes = 109               # Total number of classes (individuals)
 
 # Parameters used in functions.filter_data()
@@ -89,10 +89,7 @@ all_channels_yang = ['C1..', 'Cz..', 'C2..', 'Af3.', 'Afz.', 'Af4.', 'O1..', 'Oz
 #     gru 256 -> 129 min for training ; 4.99 seconds for testing
 #
 # lstm 128  4 units -> 37 min for training ; 1.56 seconds for testing
-# lstm 128  6 units -> XX min for training ; X.XX seconds for testing
-# lstm 128  7 units -> XX min for training ; X.XX seconds for testing
-# lstm 128  8 units -> XX min for training ; X.XX seconds for testing
-# lstm 128  9 units -> XX min for training ; X.XX seconds for testing
+# lstm 128  6 units -> 53 min for training ; 2.31 seconds for testing
 # lstm 128 10 units -> 84 min for training ; 3.57 seconds for testing
 
 # functions.create_csv_database_from_edf('./Dataset/','./Dataset_CSV/', num_classes)
