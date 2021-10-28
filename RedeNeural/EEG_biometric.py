d283fe6ef3049fe4b583eaf96424fae83aefee68
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-10-28 00:12:57 -0300

    data generator

--- RedeNeural/EEG_biometric.py
+++ RedeNeural/EEG_biometric.py
@@ -24,7 +24,7 @@ initial_learning_rate = 0.01    # Initial learning rate
 # Parameters used in functions.load_data()
 # folder_path = './Dataset_CSV/'
 folder_path = '/media/work/carlosfreitas/IniciacaoCientifica/RedeNeural/Dataset_CSV/'
-train_tasks = [1, 2]               # Tasks used for training and validation
+train_tasks = [1]               # Tasks used for training and validation
 test_tasks = [2]                # Tasks used for testing
 num_classes = 109               # Total number of classes (individuals)
 
@@ -131,13 +131,28 @@ if(option == 1):
     # Loading the raw data
     train_content, test_content = functions.load_data(folder_path, train_tasks, test_tasks, 'csv', num_classes, 1)   
 
+    print('after loading')
+    print(f'train_content[0] = {train_content[0]}')
+    print(f'train_content[109] = {train_content[109]}')
+    print(f'test_content[0] = {test_content[0]}')
+
     # Filtering the raw data
     train_content = functions.filter_data(train_content, band_pass_3, sample_frequency, filter_order, filter_type, 1)
     test_content = functions.filter_data(test_content, band_pass_3, sample_frequency, filter_order, filter_type, 1)
 
+    print('after filtering')
+    print(f'train_content[0] = {train_content[0]}')
+    print(f'train_content[109] = {train_content[109]}')
+    print(f'test_content[0] = {test_content[0]}')
+
     # Normalize the filtered data
     train_content = functions.normalize_data(train_content, 'sun', 1)
     test_content = functions.normalize_data(test_content, 'sun', 1)
+
+    print('after normalizing')
+    print(f'train_content[0] = {train_content[0]}')
+    print(f'train_content[109] = {train_content[109]}')
+    print(f'test_content[0] = {test_content[0]}')
 
     # Getting the training, validation and testing data
     # x_train, y_train, x_val, y_val = functions.crop_data(train_content, train_tasks, num_classes,
@@ -149,6 +164,7 @@ if(option == 1):
     print(f'x_train: {x_train.shape}')
     print(f'x_test: {x_test.shape}')
 
+    print('after cropping')
     print(f'x_train[0] = {x_train[0]}')
     print(f'x_train[109] = {x_train[109]}')
     print(f'x_test[0] = {x_test[0]}')
