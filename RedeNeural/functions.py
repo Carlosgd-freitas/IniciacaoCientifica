f702c67af76efdc7bc68bbf4ab2b767c952ff553
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-11-02 16:48:43 -0300

    data generator

--- RedeNeural/functions.py
+++ RedeNeural/functions.py
@@ -744,8 +744,9 @@ class DataGenerator(keras.utils.Sequence):
 
         # Find list of IDs
         # list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        list_IDs_temp = []
         for k in range(0, 11):
-            list_IDs_temp = self.list_IDs[k]
+            list_IDs_temp[k] = self.list_IDs[k]
 
         print(f'__getitem__ : list_IDs_temp = {list_IDs_temp}')
 
