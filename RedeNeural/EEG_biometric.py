bfc963dad6f637d2a4ef0e44071a826c5853c723
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-11-15 19:22:55 -0300

    data generator

--- RedeNeural/EEG_biometric.py
+++ RedeNeural/EEG_biometric.py
@@ -277,29 +277,45 @@ print(f'y_test.shape = {y_test.shape}; y_test_2.shape = {y_test_2.shape}')
 
 i = 0
 while i < len(x_train):
-    e_train_x = euclidean_distances(x_train[i], x_train_2[i])
-    print(f'i = {i}, e_train_x.diagonal() = {e_train_x.diagonal()}\n')
+    print(f'i = {i}')
+    print(f'x_train[i] = {x_train[i]}')
+    print(f'x_train_2[i] = {x_train_2[i]}\n')
     i += 1
+
+print(f'y_train = {y_train}')
+print(f'y_train_2 = {y_train_2}\n')
+
+input('enter.')
 
 i = 0
 while i < len(x_val):
-    e_val_x = euclidean_distances(x_val[i], x_val_2[i])
-    print(f'i = {i}, e_val_x.diagonal() = {e_val_x.diagonal()}\n')
+    print(f'i = {i}')
+    print(f'x_val[i] = {x_val[i]}')
+    print(f'x_val_2[i] = {x_val_2[i]}\n')
     i += 1
+
+print(f'y_val = {y_val}')
+print(f'y_val_2 = {y_val_2}\n')
+
+input('enter.')
 
 i = 0
 while i < len(x_test):
-    e_test_x = euclidean_distances(x_test[i], x_test_2[i])
-    print(f'i = {i}, e_test_x.diagonal() = {e_test_x.diagonal()}\n')
+    print(f'i = {i}')
+    print(f'x_test[i] = {x_test[i]}')
+    print(f'x_test_2[i] = {x_test_2[i]}\n')
     i += 1
 
-e_train_y = euclidean_distances(y_train, y_train_2)
-e_val_y = euclidean_distances(y_val, y_val_2)
-e_test_y = euclidean_distances(y_test, y_test_2)
+print(f'y_test = {y_test}')
+print(f'y_test_2 = {y_test_2}\n')
 
-print(f'e_train_y.diagonal() = {e_train_y.diagonal()}\n')
-print(f'e_val_y.diagonal() = {e_val_y.diagonal()}\n')
-print(f'e_test_y.diagonal() = {e_test_y.diagonal()}\n')
+# e_train_y = euclidean_distances(y_train, y_train_2)
+# e_val_y = euclidean_distances(y_val, y_val_2)
+# e_test_y = euclidean_distances(y_test, y_test_2)
+
+# print(f'e_train_y.diagonal() = {e_train_y.diagonal()}\n')
+# print(f'e_val_y.diagonal() = {e_val_y.diagonal()}\n')
+# print(f'e_test_y.diagonal() = {e_test_y.diagonal()}\n')
 
 sys.exit()
 
